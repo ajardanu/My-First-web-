14aeed4333af01e57f076cbad07abd93e456525e
ajardanu <danumadw@gmail.com>
2024-05-27 18:52:34 +0700

    first commit

---
+++ node.js
@@ -0,0 +1,9 @@
+const nav3 = document.querySelector('.menu nav ul li a');
+document.querySelectorAll('.menu nav ul li a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
